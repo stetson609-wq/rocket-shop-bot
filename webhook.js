3d8b9a28a72063000b183ab66c15a362a8868b17
stetson609-wq <stetson609@gmail.com>
2026-03-15 11:40:59 -0500

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,29 +1,29 @@
 import axios from "axios"
 
-export async function sendWebhook(webhook, item) {
+export async function sendAlert(webhook,item){
 
-  try {
+ try{
 
-    await axios.post(webhook, {
-      embeds: [
-        {
-          title: "Wishlist Item Found!",
-          description: `**${item.paint} ${item.name}** is in the shop`,
-          image: { url: item.image },
-          fields: [
-            {
-              name: "Price",
-              value: `${item.price} Credits`,
-              inline: true
-            }
-          ],
-          color: 5763719
-        }
-      ]
-    })
+ await axios.post(webhook,{
+  embeds:[
+   {
+    title:"Wishlist Item Found",
+    description:`${item.paint} ${item.name} is in the shop`,
+    image:{url:item.image},
+    fields:[
+     {
+      name:"Price",
+      value:`${item.price} Credits`,
+      inline:true
+     }
+    ],
+    color:5763719
+   }
+  ]
+ })
 
-  } catch (err) {
-    console.error("Webhook failed:", err.message)
-  }
+ }catch(e){
+  console.log("Webhook error",e.message)
+ }
 
 }
